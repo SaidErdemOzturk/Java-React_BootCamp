f04e27ef0292d5cdbf12b01a3fd6daf1eea7ad41
Said Erdem Öztürk <saiderdemozturk@gmail.com>
2022-08-17 15:04:42 +0300

    added getByEmail method

--- backend/Odevler/Gun_7/Odev_1/hrms/src/main/java/javaReact_BootCamp/hrms/dataAccess/abstracts/UserDao.java
+++ backend/Odevler/Gun_7/Odev_1/hrms/src/main/java/javaReact_BootCamp/hrms/dataAccess/abstracts/UserDao.java
@@ -6,4 +6,6 @@ import javaReact_BootCamp.hrms.entities.concretes.User;
 
 public interface UserDao extends JpaRepository<User, Integer>{
 	User getByEmailAndPassword(String email, String password);
+	
+	User getByEmail(String email);
 }
